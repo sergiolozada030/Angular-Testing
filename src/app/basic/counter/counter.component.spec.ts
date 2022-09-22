a231200def481f94c12c35398102dbd5488142c4
Sergio Andrés Lozada Perea <sergio.lozada@pragma.com.co>
2022-09-22 17:37:10 -0500

    pruebas a counter

--- src/app/basic/counter/counter.component.spec.ts
+++ src/app/basic/counter/counter.component.spec.ts
@@ -24,4 +24,29 @@ describe('CounterComponent', () => {
   it('Debe hacer match con el snapshot', () => {
     expect(compiled.innerHTML).toMatchSnapshot();
   });
+
+  it('increseBy basado en el argumento', () => {
+    component.increaseBy(1);
+    expect(component.counter).toEqual(1);
+  });
+
+  it('Al hacer click debe aumentar o decrementar el valor', () => {
+    const buttons = compiled.querySelectorAll('button');
+    buttons[0].click();
+
+    expect(component.counter).toEqual(1);
+
+    buttons[1].click();
+    buttons[1].click();
+
+    expect(component.counter).toEqual(-1);
+  });
+
+  it('Al ejecutar increaseBy actualiza el html', () => {
+    component.increaseBy(2);
+    fixture.detectChanges();
+
+    const h1 = compiled.querySelector('h2');
+    expect(h1?.textContent).toContain('2');
+  });
 });
