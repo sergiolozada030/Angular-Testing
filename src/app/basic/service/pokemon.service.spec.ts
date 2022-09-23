ae496206e2687f7eed7c88c84c9f81daf4375cdc
Sergio Andrés Lozada Perea <sergio.lozada@pragma.com.co>
2022-09-23 10:21:57 -0500

    pruebas en service pokemon

--- src/app/basic/service/pokemon.service.spec.ts
+++ src/app/basic/service/pokemon.service.spec.ts
@@ -1,16 +1,25 @@
+import { HttpClientModule } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
-
 import { PokemonService } from './pokemon.service';
 
 describe('PokemonService', () => {
   let service: PokemonService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientModule],
+    });
     service = TestBed.inject(PokemonService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('Debe traer la informacion del pokemon charizard', (done) => {
+    service.getPokemon(6).subscribe((pokemon) => {
+      expect(pokemon.name).toEqual('charizard!');
+      done();
+    });
   });
 });
